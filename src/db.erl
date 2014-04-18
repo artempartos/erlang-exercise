04a924b1961800a5ca77451d1ad869e01db18cdb
Artem Petrov <partos0511@gmail.com>
2014-04-18 11:45:28 +0400

    finish db implementation

---
+++ src/db.erl
@@ -0,0 +1,27 @@
+-module(db).
+-import(mylist,[filter/2]).
+-export([new/0, destroy/1, write/3, delete/2, read/2, values/2]).
+
+new() -> [].
+
+destroy(_Db) -> ok.
+
+write(Db, K, V) ->
+	[{K,V}] ++ filter_key(Db, K).
+
+delete(Db, K) ->
+	filter_key(Db, K).
+
+read([], _K) -> nil;
+read([{K, V}|_T], K) -> V;
+read([_H|T], K) -> read(T,K).
+
+filter_key(Db, K) ->
+	[{K1,_V} || {K1, _V} <- Db, K1 /= K].
+
+values([], _V) -> [];
+values([{K, V}|T], V) -> [K] ++ values(T,V);
+values([_H|T], V) -> values(T,V).
+
+
+
---
+++ test/db_tests.erl
@@ -0,0 +1,69 @@
+-module(db_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+
+new_test() ->
+	?assertEqual([], db:new()).
+
+destroy_test() ->
+	Db = db:new(),
+	?assertEqual(ok, db:destroy(Db)).
+
+write_test() ->
+  Db = db:new(),
+  State1 = db:write(Db, k1, v1),
+  ?assertEqual([{k1, v1}], State1),
+
+  State2 = db:write(State1, k2, v2),
+  ?assertEqual([{k2, v2}, {k1, v1}], State2),
+
+  State3 = db:write(State2, k1, v3),
+  ?assertEqual([{k1, v3}, {k2, v2}], State3).
+
+delete_test() ->
+  Db = db:new(),
+  State1 = db:write(Db, k1, v1),
+  ?assertEqual([{k1, v1}], State1),
+
+  State2 = db:write(State1, k2, v2),
+  ?assertEqual([{k2, v2}, {k1, v1}], State2),
+
+  State3 = db:delete(State2, k3),
+  ?assertEqual([{k2, v2}, {k1, v1}], State2),
+
+  State4 = db:delete(State3, k1),
+  ?assertEqual([{k2, v2}], State4).
+
+read_test() ->
+  Db = db:new(),
+  State1 = db:write(Db, k1, v1),
+  ?assertEqual([{k1, v1}], State1),
+
+  State2 = db:write(State1, k2, v2),
+  ?assertEqual([{k2, v2}, {k1, v1}], State2),
+
+  Val1 = db:read(State2, k3),
+  ?assertEqual(nil, Val1),
+
+  Val2 = db:read(State2, k1),
+  ?assertEqual(v1, Val2).
+
+values_test() ->
+  Db = db:new(),
+  State1 = db:write(Db, k1, v1),
+  ?assertEqual([{k1, v1}], State1),
+
+  State2 = db:write(State1, k2, v2),
+  ?assertEqual([{k2, v2}, {k1, v1}], State2),
+
+  State3 = db:write(State2, k3, v2),
+  ?assertEqual([{k3,v2},{k2, v2}, {k1, v1}], State3),
+
+  Val1 = db:values(State3, v2),
+  ?assertEqual([k3,k2], Val1),
+
+  Val2 = db:values(State3, v1),
+  ?assertEqual([k1], Val2),
+
+  Val3 = db:values(State3, v3),
+  ?assertEqual([], Val3).
